8b012d4c94bdcb5b0d24affd9fe1881dc4883425
Matej Fartelj <matej@luminet.si>
2020-12-21 20:57:16 +0100

    added tray limitation to ExperimentRequest

--- SampleAPI/Models/ExperimentRequest.cs
+++ SampleAPI/Models/ExperimentRequest.cs
@@ -4,6 +4,8 @@ namespace SampleAPI.Models
 {
 	public class ExperimentRequest
 	{
+		public int AllowedPlates { get; set; }
+
 		public IEnumerable<Experiment> Experiments { get; set; }
 
 		public int TraySize { get; set; }
